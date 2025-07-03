43b0191ff65e8a48a870245332baa99be25711d9
ndestrieux <n.destrieux@gmail.com>
2025-07-03 14:49:19 -0400

    Error handling added.

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -4,6 +4,16 @@ from typing import Dict
 import requests
 
 
+BLANK_EMOTION_RESPONSE = {
+    "anger": None,
+    "fear": None,
+    "joy": None,
+    "sadness": None,
+    "disgust": None,
+    "dominant_emotion": None,
+}
+
+
 def dominant_emotion(emotions: Dict[str, float]) -> str:
     return max(emotions, key=emotions.get)
 
@@ -13,6 +23,8 @@ def emotion_detector(text_to_analyze: str) -> Dict[str, float | str]:
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     data = {"raw_document": {"text": text_to_analyze}}
     response = requests.post(url, headers=headers, json=data, timeout=10)
+    if response.status_code == 400:
+        return BLANK_EMOTION_RESPONSE
     response_json = json.loads(response.text)
     emotions = response_json["emotionPredictions"][0]["emotion"]
     emotions.update({"dominant_emotion": dominant_emotion(emotions)})
--- server.py
+++ server.py
@@ -9,8 +9,10 @@ app = Flask("Emotions")
 def analyze_emotion():
     text_to_analyze = request.args.get("textToAnalyze")
     result = emotion_detector(text_to_analyze)
+    if not result.get("dominant_emotion"):
+        return "Invalid text! Please try again!"
     return f"""For the given statement, the system response is 'anger': {result['anger']},
-    disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy': {result['joy']},
+    'disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy': {result['joy']},
     'sadness': {result['sadness']}. The dominant emotion is {result['dominant_emotion']}."""
 
 
